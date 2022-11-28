865eebaacb552506a447877f19593578cf3f152e
Scott Sutherland <scottsut@live.com>
2022-11-28 10:14:59 -0800

    Fixing setup

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from Cython.Distutils import build_ext
 import numpy
 
 setup(
-	maintainer='Corey Lynch',
+    maintainer='Corey Lynch',
     name='pyfm',
     packages=find_packages(),
     url='https://github.com/coreylynch/pyFM',
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("pyfm_fast", ["pyfm_fast.pyx"],
-    						 libraries=["m"],
-    						 include_dirs=[numpy.get_include()])]
+    include_dirs=[numpy.get_include()])]
 )
